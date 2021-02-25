595c75fa0702f561430de5edf465e7bf67011e61
tibin.zhang <tibin.zhang@huifu.com>
2021-02-25 11:25:05 +0800

    Add git submit log

--- app/src/main/java/com/example/androidlearn/MainActivity.java
+++ app/src/main/java/com/example/androidlearn/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //添加Git提交记录
         setContentView(R.layout.activity_main);
     }
 }
